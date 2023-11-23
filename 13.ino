316a81c49817def3c7929eb4bd991c787fe029f0
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2023-11-23 20:33:51 +0900

    Create 13.ino

---
+++ 13.ino
@@ -0,0 +1,25 @@
+#include <dht.h>                  //dht.zip 라이브러리를 설치
+#define DHT22_PIN 7             //D7 연결
+
+dht DHT;
+
+float temperature ;
+float humidity ;
+
+void setup()
+{
+  Serial.begin(9600);             //시리얼모니터 출력
+}
+
+void loop()
+{
+  float chk = DHT.read22(DHT22_PIN); 
+
+  temperature = DHT.temperature;         //온도
+  humidity = DHT.humidity;                //상대습도
+  
+  Serial.print(temperature, 1);              //소숫점 1자리만 출력 
+  Serial.print(", ");        
+  Serial.println(humidity, 1);                  //소숫점 1자리만 출력
+  delay(1000);                              //1초마다 반복 실행
+}
